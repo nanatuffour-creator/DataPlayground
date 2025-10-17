4df3e644cd3f434d1e8db7c130f70b02694ea336
nanayaw246-max <johntuffourannan@gmail.com>
2025-10-17 14:19:09 +0000

    Update binaries and assembly information for CollectionIntroduction

--- CollectionIntroduction/CollectionIntroduction/obj/Debug/net8.0/CollectionIntroduction.AssemblyInfo.cs
+++ CollectionIntroduction/CollectionIntroduction/obj/Debug/net8.0/CollectionIntroduction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollectionIntroduction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21add193bda78c38019946ac9ae4b54a302d7ebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51d16d2c0b2c1a8d8a418eb7c8569e984fcbc1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollectionIntroduction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollectionIntroduction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
