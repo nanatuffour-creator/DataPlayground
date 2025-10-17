33eb2a5abb9401006acad7b3303568e3b41c2660
nanayaw246-max <johntuffourannan@gmail.com>
2025-10-17 17:06:20 +0000

    Refactor array declaration to use string type and comment out unused code

--- CollectionIntroduction/CollectionIntroduction/Program.cs
+++ CollectionIntroduction/CollectionIntroduction/Program.cs
@@ -1,58 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// // Console.WriteLine("Hello, World!");
 ONE:
 Console.Write("Enter size for array : ");
 bool parsed = int.TryParse(Console.ReadLine(), out int x);
 
 if (!parsed) { Console.WriteLine("You entered the wrong value.It should be a number.");goto ONE;}
 
-int[] arr = new int[x];
+// int[] arr = new int[x];
 
-//arr[0] = 1;
-int j;
+// //arr[0] = 1;
+// int j;
 
-int value = 0;
-for (j = 0; j < x; j++)
-{
-TWO:
-    Console.WriteLine("Enter values for the array : ");
-    string? m = Console.ReadLine();
-    // int value = 0;
-    bool valueParsed = int.TryParse(m, out value);
-    if (!valueParsed)
-    {
-        Console.WriteLine("You entered the wrong value.It should be a number.");
-        goto TWO;
-    }
+// int value = 0;
+// for (j = 0; j < x; j++)
+// {
+// TWO:
+//     Console.WriteLine("Enter values for the array : ");
+//     string? m = Console.ReadLine();
+//     // int value = 0;
+//     bool valueParsed = int.TryParse(m, out value);
+//     if (!valueParsed)
+//     {
+//         Console.WriteLine("You entered the wrong value.It should be a number.");
+//         goto TWO;
+//     }
 
-    arr[j] = value;
+//     arr[j] = value;
 
-}
+// }
 
-Console.WriteLine();
+// Console.WriteLine();
 
-for (int i = 0; i < x; i++)
-{
-    Console.WriteLine(arr[i]);
-}
+// for (int i = 0; i < x; i++)
+// {
+//     Console.WriteLine(arr[i]);
+// }
 
-Console.WriteLine();
+// Console.WriteLine();
 
-if (x == 1)
-{
-    Console.WriteLine($"Add : {arr[0]}");
-}
-else { Console.WriteLine($"Add : {arr[0] + value}"); }
+// if (x == 1)
+// {
+//     Console.WriteLine($"Add : {arr[0]}");
+// }
+// else { Console.WriteLine($"Add : {arr[0] + value}"); }
 
-if (x % 2 == 0)
-{
-    Console.WriteLine($"Middle Numbers : {arr[(x / 2) - 1]} , {arr[x / 2]}");
-}
-if(x == 2)
-{
-    Console.WriteLine($"There is no middle number.");
-}
-else
-{
-    Console.WriteLine($"Middle Number : {arr[x / 2]}");
-}
+// if(x == 2)
+// {
+//     Console.WriteLine($"There is no middle number.");
+// }
+// else if (x % 2 == 0)
+// {
+//     Console.WriteLine($"Middle Numbers : {arr[(x / 2) - 1]} , {arr[x / 2]}");
+// }
+// else
+// {
+//     Console.WriteLine($"Middle Number : {arr[x / 2]}");
+// }
+
+
+
+string[] arr = new string[x];
